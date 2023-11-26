60a7516d39ef338dd312337768658f2b9e383dc5
Robinson Ortega <rkortega1994@gmail.com>
2023-11-26 14:45:23 +0100

    Update Classroom model and API route

---
+++ src/app/api/classroom/[id]/route.ts
@@ -0,0 +1,89 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
+import {
+  SuccessResponse,
+  ErrorResponse,
+} from "@/interfaces/response.interface";
+
+interface Params {
+  params: {
+    id: string;
+  };
+}
+
+export async function GET(request: Request, { params }: Params) {
+  try {
+    const { id } = params;
+    const classroom = await prisma.classroom.findUnique({
+      where: { id: id },
+    });
+
+    if (!classroom)
+      return NextResponse.json(ErrorResponse.json("Classroom not found", []), {
+        status: 404,
+      });
+
+    return NextResponse.json(
+      SuccessResponse.json("Classroom retrieved", classroom)
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      ErrorResponse.json("Error getting classroom", [error.message]),
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(request: Request, { params }: Params) {
+  try {
+    const { id } = params;
+    const { name, capacity } = await request.json();
+
+    const classroom = await prisma.classroom.findUnique({
+      where: { id: id },
+    });
+
+    if (!classroom)
+      return NextResponse.json(ErrorResponse.json("Classroom not found", []), {
+        status: 404,
+      });
+
+    const updatedClassroom = await prisma.classroom.update({
+      where: { id: id },
+      data: { name, capacity },
+    });
+
+    return NextResponse.json(
+      SuccessResponse.json("Classroom updated", updatedClassroom)
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      ErrorResponse.json("Error updating classroom", [error.message]),
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(request: Request, { params }: Params) {
+  try {
+    const { id } = params;
+
+    const classroom = await prisma.classroom.findUnique({
+      where: { id: id },
+    });
+
+    if (!classroom)
+      return NextResponse.json(ErrorResponse.json("Classroom not found", []), {
+        status: 404,
+      });
+
+    await prisma.classroom.delete({ where: { id: id } });
+
+    return NextResponse.json(SuccessResponse.json("Classroom deleted", []));
+  } catch (error: any) {
+    return NextResponse.json(
+      ErrorResponse.json("Error deleting classroom", [error.message]),
+      { status: 500 }
+    );
+  }
+}
--- src/app/api/classroom/route.ts
+++ src/app/api/classroom/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { generateCode } from "@/libs/generators";
 import { prisma } from "@/libs/prisma";
 import {
@@ -6,23 +6,45 @@ import {
   ErrorResponse,
 } from "@/interfaces/response.interface";
 
-export async function GET() {
-  return NextResponse.json({ message: "getting classrooms..." });
+/**
+ * GET /api/classroom
+ *
+ * @returns {NextResponse}
+ */
+export async function GET(request: NextRequest) {
+  try {
+    // get page from query params
+    const url = new URL(request.url);
+
+    const page = Number(url.searchParams.get("page")) || 1;
+
+    const classrooms = await prisma.classroom.findMany();
+
+    return NextResponse.json(
+      SuccessResponse.json("Classrooms retrieved", classrooms)
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      ErrorResponse.json("Error getting classrooms", [error.message]),
+      { status: 500 }
+    );
+  }
 }
 
+/**
+ * POST /api/classroom
+ *
+ * @param {Request} request
+ *
+ * @returns {NextResponse}
+ */
 export async function POST(request: Request) {
   try {
     const { name, capacity } = await request.json();
     const code = generateCode();
-    console.log(code);
-    console.log({ name, capacity });
 
     const newClassroom = await prisma.classroom.create({
-      data: {
-        name,
-        capacity,
-        code,
-      },
+      data: { name, capacity, code },
     });
 
     return NextResponse.json(
