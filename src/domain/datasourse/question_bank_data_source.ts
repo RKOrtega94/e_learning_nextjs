f698d6cb7ec25b49c0a2b3569517a6bc24601e98
Robinson Ortega <rkortega1994@gmail.com>
2023-12-26 03:36:11 +0100

    Refactor Classroom and QuestionBank models

--- src/domain/datasourse/question_bank_data_source.ts
+++ src/domain/datasourse/question_bank_data_source.ts
@@ -1,6 +1,6 @@
 import { IQuestionBank } from "@interface/_interfaces";
 
-export default abstract class QuestionRepository {
+export default abstract class QuestionBankDataSource {
   abstract getAll(): Promise<IQuestionBank[]>;
   abstract create(question: IQuestionBank): Promise<IQuestionBank>;
 }
