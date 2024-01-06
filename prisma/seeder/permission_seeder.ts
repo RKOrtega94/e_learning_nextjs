4e708c8f68c59fa1ed2661aaa44ae6ca061b26c7
Robinson Ortega <rkortega1994@gmail.com>
2024-01-06 04:25:01 +0100

    Add classroom permissions to permission seeder

--- prisma/seeder/permission_seeder.ts
+++ prisma/seeder/permission_seeder.ts
@@ -1,7 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
 export default async function permissionSeeder(prisma: PrismaClient) {
-  const permissions = [{ name: "create_classroom" }];
+  const permissions = [
+    // Classroom permissions
+    { name: "create_classroom" },
+    { name: "read_classroom" },
+    { name: "update_classroom" },
+    { name: "delete_classroom" },
+  ];
 
   permissions.forEach(async (permission) => {
     const checkPermission = await prisma.permission.findMany({
