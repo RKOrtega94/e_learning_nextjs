d2224a25752c6093511a7dd26991845e22dd3d76
Robinson Ortega <rkortega1994@gmail.com>
2024-01-07 02:51:47 +0100

    Add icons and buttons to ClassroomSections and EvaluationSection

--- src/app/(main)/dashboard/components/sections/ClassroomSections.tsx
+++ src/app/(main)/dashboard/components/sections/ClassroomSections.tsx
@@ -1,12 +1,18 @@
 import { Card } from "@/presentation/components";
+import { SiGoogleclassroom } from "react-icons/si";
+import { IoAddCircleSharp } from "react-icons/io5";
 
 export default function ClassroomSections() {
   return (
     <Card className="flex flex-col gap-4">
       <div className="flex justify-between items-center">
-        <h3 className="text-xl font-bold">My Classrooms</h3>
+        <h3 className="text-xl font-bold flex items-center">
+          <SiGoogleclassroom className="inline-block mr-2" />
+          My Classrooms
+        </h3>
         <button className="bg-green-500 dark:bg-green-900 px-4 py-2 rounded-md shadow hover:shadow-md text-sm font-bold">
           Add classroom
+          <IoAddCircleSharp className="inline-block ml-2" />
         </button>
       </div>
     </Card>
--- src/app/(main)/dashboard/components/sections/EvaluationSection.tsx
+++ src/app/(main)/dashboard/components/sections/EvaluationSection.tsx
@@ -1,6 +1,7 @@
 import { Card } from "@/presentation/components";
 import EmptyCard from "@/presentation/components/card/EmptyCard";
 import Link from "next/link";
+import { IoChevronForwardSharp } from "react-icons/io5";
 
 export default function EvaluationSection() {
   return (
@@ -8,10 +9,11 @@ export default function EvaluationSection() {
       <div className="flex justify-between items-center">
         <h3 className="text-xl font-bold">Evaluations</h3>
         <Link
-          className="text-sm font-bold text-blue-500 dark:text-blue-400 hover:underline"
+          className="text-sm font-bold text-blue-500 dark:text-blue-400 hover:underline flex items-center"
           href={`/evaluations`}
         >
           See all
+          <IoChevronForwardSharp className="inline-block ml-2" />
         </Link>
       </div>
       <EmptyCard message="No evaluations yet." />
